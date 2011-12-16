1d12a67436b97a5ce191e2a7825d8544f7910573
Josh Kramer <joshiek1990@gmail.com>
2011-12-15 20:36:19 -0500

    Merge conflict resolution

--- js/PageView.class.js
+++ js/PageView.class.js
@@ -36,44 +36,21 @@ function PageView(containerEleArg, currentPageArg){
 	this.updatePage = function(pageNumber){
 		if(pageNumber !== undefined)
 			this.currentPage = pageNumber;
-<<<<<<< HEAD
-		
-		//Update both pageNav objects
-		for(var i = 0; i < 2; i++){
-			pageNav[i].updateCurrentPage(this.currentPage);
-=======
+
 
 		//Update both pageNav objects
 		for(var i = 0; i < 2; i++){
 			pageNav[i].setPage(this.currentPage);
->>>>>>> Hash
+
 		}
 		
 		//update postList
 		postList.getPosts(this.currentPage);
 	}
 	
-<<<<<<< HEAD
-
-}
 
 
 
-
-$(document).ready(function(){
-	var pageView = new PageView('#postContainer');
-	setTimeout(function(){
-		alert('t');
-		pageView.updatePage(2);}, 2000);
-
-});
-/*
-
-	Paginate.go('previous'); will bring the page list to the previous page
-	Paginate.go('next'); go to next page
-	Paginate.go(3434); go to page 3434
-*/
-=======
 	//for Hash class, to tell if someone is trying to go to a non-existant page
 	this.getMaxPage = function(){
 		return pageNav[0].getMaxPage();
@@ -81,4 +58,4 @@ $(document).ready(function(){
 	
 
 }
->>>>>>> Hash
+
